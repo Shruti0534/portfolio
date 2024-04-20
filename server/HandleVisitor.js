b403963d6df2b2fff6b35a3b4d3e505e5178695b
Shruti0534 <shrutit515@gmail.com>
2024-04-20 19:26:52 +0530

    ccc

--- server/HandleVisitor.js
+++ server/HandleVisitor.js
@@ -1,8 +1,15 @@
-
 'use server'
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+if(!global.prisma){
+    global.prisma = new PrismaClient()
+}
+
+const prisma = global.prisma
+
+if(process.env.NODE_ENV != 'production')    
+    global.prisma = prisma
+
  export const toIncreaseCount= async(previouscount)=>{
     try {
         const mcount=await prisma.visitor.update({
